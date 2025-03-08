a3a5bda975d6bd4c420697c8b44e08f084e79490
Gin-sen <maxime.places@gmail.com>
2025-03-08 03:38:48 +0100

    Add XML documentation to DummyController and its methods for improved clarity

--- Ginsen.Net8.Async.Milestone.Api/Controllers/DummyController.cs
+++ Ginsen.Net8.Async.Milestone.Api/Controllers/DummyController.cs
@@ -4,17 +4,29 @@ using Asp.Versioning;
 
 namespace Ginsen.Net8.Async.Milestone.Api.Controllers
 {
+  /// <summary>
+  /// Controller for handling dummy operations.
+  /// </summary>
   [ApiVersion(1)]
   [Route("api/v{v:apiVersion}/[controller]")]
   [ApiController]
   public class DummyController : ControllerBase
   {
     private readonly ILogger<DummyController> _logger;
+
+    /// <summary>
+    /// Controller for handling dummy operations.
+    /// </summary>
     public DummyController(ILogger<DummyController> logger)
     {
       _logger = logger;
     }
 
+    /// <summary>
+    /// Get a dummy object.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpGet("GetOk")]
     [Produces( "application/json" )]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetDummy))]
